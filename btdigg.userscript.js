9355bac3946ab9444c4c832e82f0c387c34319c7
Leigh Zhu <i@zhuli.me>
2015-08-24 20:29:28 +0800

    use GM_addStyle()

--- btdigg.userscript.js
+++ btdigg.userscript.js
@@ -5,7 +5,7 @@
 // @description  fast select and copy magnet
 // @author       Leigh Zhu
 // @match        http://btdigg.org/search?*
-// @grant        none
+// @grant        GM_addStyle
 // ==/UserScript==
 
 ;(function(window, document) {
@@ -19,9 +19,9 @@
   };
 
   document.querySelector('body').innerHTML = document.querySelector('body').innerHTML +
-    '<div id="links-panel" style="position: fixed;top: 10%;right: 2%;box-shadow: 0 5px 10px #ddd;border: 1px solid #02a3c6;">' +
-      '<button id="btn-reset" style="position: absolute;top: -20px;height: 20px;border-radius: 0;background: #fff;border: 1px solid #02a3c6;border-bottom: none;left: -1px;"> Reset </button>' +
-      '<textarea id="magnet-links" cols="100" rows="10" style="border: 1px solid #f8f8f8;background: rgba(230, 230, 230, .9)"></textarea>' +
+    '<div id="links-panel">' +
+      '<button id="btn-reset"> Reset </button>' +
+      '<textarea id="magnet-links" cols="100" rows="10"></textarea>' +
     '</div>';
 
   var linksBox = document.getElementById('magnet-links');
@@ -43,8 +43,7 @@
 
         var ipt = document.createElement('input');
         ipt.setAttribute('type', 'checkbox');
-        ipt.style.fontSize = '100%';
-        ipt.style.marginRight = '.5em';
+        ipt.setAttribute('class', 'checkbox');
         ipt.addEventListener('change', function(e) {
           if (e.target.checked === true) {
             selectedLinks.push(magnet);
@@ -64,6 +63,12 @@
 
   document.getElementById('btn-reset').addEventListener('click', function() {
     linksBox.value = '';
+    localStorage.setItem('links', '');
   });
 
 })(window, document);
+
+GM_addStyle('#links-panel { position: fixed;top: 10%;right: 2%;box-shadow: 0 5px 10px #ddd;border: 1px solid #02a3c6; }');
+GM_addStyle('#btn-reset { position: absolute;top: -20px;height: 20px;border-radius: 0;background: #fff;border: 1px solid #02a3c6;border-bottom: none;left: -1px; }');
+GM_addStyle('#magnet-links { border: 1px solid #f8f8f8;background: rgba(230, 230, 230, .9) }');
+GM_addStyle('.checkbox { font-size: 100%; margin-right: .5em; }');
